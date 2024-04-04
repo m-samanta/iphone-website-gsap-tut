1ef5bf1d2ee9cc7721fa8aca235f79d1b51ee08e
Mriganka Samanta <m.samanta.dev@gmail.com>
2024-04-03 22:02:10 -0400

    Features

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Navbar from "./components/Navbar";
 import Model from "./components/Model";
+import Features from "./components/Features";
 
 import * as Sentry from "@sentry/react";
 
@@ -12,6 +13,7 @@ const App = () => {
       <Hero />
       <Highlights />
       <Model />
+      <Features />
     </main>
   );
 };
